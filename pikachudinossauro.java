99cb2ac31ad11cb1bf1a4ba9d6a1e9f970fb9835
Laura <kwillsf9@gmail.com>
2024-11-12 14:36:32 -0300

    Update pikachudinossauro.java

--- pikachudinossauro.java
+++ pikachudinossauro.java
@@ -1,33 +1,147 @@
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
 
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+public class GamePanel extends JPanel implements ActionListener, KeyListener {
+    private int boardWidth = 750;
+    private int boardHeight = 250;
 
-import java.swing.*;
+    // Pikachu e Obstáculos
+    private Block pikachu;
+    private ArrayList<Block> obstacles;
 
-
-public class PikachuDinossauro extends JPanel {
-    int boardWidth = 750;
-    int boardHeight = 250;
-
-//midia >:)
-private ImageIcon pikachuparado = new ImageIcon("src\\imagens\\pikachu.png");
-
-public PikachuDinossauro() {
-    // Cria um JLabel com a imagem 'pikachuparado'
-    JLabel labelPikachu = new JLabel(pikachuparado);
+    // Física do jogo
+    private int velocityX = -12;
+    private int velocityY = 0;
+    private int gravity = 1;
     
-    // Configurações da janela ou painel, adicionando o JLabel
-    JFrame frame = new JFrame("Jogo do Pikachu");
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setSize(750, 250);
-    frame.add(labelPikachu);
-    frame.setVisible(true);
-}
-    
+    private boolean gameOver = false;
+    private int score = 0;
 
+    private Timer gameLoop;
+    private Timer placeObstacleTimer;
+
+    // Imagens
+    private Image pikachuRunImg, pikachuDeadImg, pikachuJumpImg, ekansImg, caliopeGengarImg;
+
+    public GamePanel() {
+        setPreferredSize(new Dimension(boardWidth, boardHeight));
+        setBackground(Color.lightGray);
+        setFocusable(true);
+        addKeyListener(this);
+
+        loadImages();
+        initializeGame();
+        
+        // Configura o timer do jogo
+        gameLoop = new Timer(1000 / 60, this);
+        gameLoop.start();
+
+        // Timer para gerar obstáculos
+        placeObstacleTimer = new Timer(1500, e -> placeObstacle());
+        placeObstacleTimer.start();
+    }
+
+    private void loadImages() {
+        pikachuRunImg = new ImageIcon("src/imagens/pikachurun.gif").getImage();
+        pikachuDeadImg = new ImageIcon("src/imagens/pikachumorto.png").getImage();
+        pikachuJumpImg = new ImageIcon("src/imagens/pikachujump.png").getImage();
+        ekansImg = new ImageIcon("src/imagens/ekans.png").getImage();
+        caliopeGengarImg = new ImageIcon("src/imagens/calipegengar.png").getImage();
+    }
+
+    private void initializeGame() {
+        pikachu = new Block(50, boardHeight - 50, 60, 50, pikachuRunImg);
+        obstacles = new ArrayList<>();
+    }
+
+    private void placeObstacle() {
+        if (gameOver) return;
+
+        double chance = Math.random();
+        if (chance > 0.5) {
+            obstacles.add(new Block(700, boardHeight - 50, 65, 50, ekansImg));
+        } else {
+            obstacles.add(new Block(700, boardHeight - 50, 70, 50, caliopeGengarImg));
+        }
+
+        if (obstacles.size() > 10) {
+            obstacles.remove(0);
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    private void draw(Graphics g) {
+        g.drawImage(pikachu.img, pikachu.x, pikachu.y, pikachu.width, pikachu.height, null);
+
+        for (Block obstacle : obstacles) {
+            g.drawImage(obstacle.img, obstacle.x, obstacle.y, obstacle.width, obstacle.height, null);
+        }
+
+        g.setColor(Color.black);
+        g.setFont(new Font("Courier", Font.PLAIN, 32));
+        g.drawString(gameOver ? "Game Over: " + score : String.valueOf(score), 10, 35);
+    }
+
+    private void move() {
+        velocityY += gravity;
+        pikachu.y += velocityY;
+
+        if (pikachu.y > boardHeight - pikachu.height) {
+            pikachu.y = boardHeight - pikachu.height;
+            velocityY = 0;
+            pikachu.img = pikachuRunImg;
+        }
+
+        for (Block obstacle : obstacles) {
+            obstacle.x += velocityX;
+            if (collision(pikachu, obstacle)) {
+                gameOver = true;
+                pikachu.img = pikachuDeadImg;
+            }
+        }
+
+        if (!gameOver) score++;
+    }
+
+    private boolean collision(Block a, Block b) {
+        return a.x < b.x + b.width && a.x + a.width > b.x &&
+               a.y < b.y + b.height && a.y + a.height > b.y;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        move();
+        repaint();
+        if (gameOver) {
+            placeObstacleTimer.stop();
+            gameLoop.stop();
+        }
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+            if (pikachu.y == boardHeight - pikachu.height) {
+                velocityY = -17;
+                pikachu.img = pikachuJumpImg;
+            }
+            if (gameOver) {
+                initializeGame();
+                score = 0;
+                gameOver = false;
+                gameLoop.start();
+                placeObstacleTimer.start();
+            }
+        }
+    }
+
+    @Override public void keyTyped(KeyEvent e) {}
+    @Override public void keyReleased(KeyEvent e) {}
 }
